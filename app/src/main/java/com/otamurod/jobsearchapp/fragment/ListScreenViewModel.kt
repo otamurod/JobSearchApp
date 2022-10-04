a710f891b076037323c12a9509a47c6fda93cb10
otamurod <o.safarov@student.inha.uz>
2022-10-04 23:04:31 +0500

    feat: create function to send query to API

--- app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenViewModel.kt
+++ app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenViewModel.kt
@@ -2,15 +2,29 @@ package com.otamurod.jobsearchapp.fragment
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.otamurod.jobsearchapp.model.ListItemModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListScreenViewModel : ViewModel() {
-    private lateinit var liveData: MutableLiveData<String>
+    private lateinit var liveData: MutableLiveData<ListItemModel>
 
     init {
         liveData = MutableLiveData()
     }
 
-    fun getLiveData(): MutableLiveData<String> {
+    fun getLiveData(): MutableLiveData<ListItemModel> {
         return liveData
+    }
+
+    fun sendQuery(query: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val retrofitService =
+                RetrofitInstance.getRetrofitInstance().create(RetrofitService::class.java)
+            val response = retrofitService.getDataFromAPI()
+            liveData.postValue(response)
+
+        }
     }
 }
