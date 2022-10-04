a8c08b5da6eae18aa37115a820105fc446d1d8af
otamurod <o.safarov@student.inha.uz>
2022-10-04 23:04:09 +0500

    feat: create function to check network state

--- app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenFragment.kt
+++ app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenFragment.kt
@@ -1,13 +1,21 @@
 package com.otamurod.jobsearchapp.fragment
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import com.otamurod.jobsearchapp.adapter.ListScreenAdapter
 import com.otamurod.jobsearchapp.databinding.FragmentListScreenBinding
+import com.otamurod.jobsearchapp.model.ListItemModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +42,8 @@ class ListScreenFragment : Fragment() {
 
     lateinit var listScreenBinding: FragmentListScreenBinding
     lateinit var viewModel: ListScreenViewModel
+    lateinit var adapter: ListScreenAdapter
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,11 +54,74 @@ class ListScreenFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        viewModel = ViewModelProviders.of(this).get(ListScreenViewModel::class.java)
-        viewModel.getLiveData()
-
+        initViewModel()
+        initAdapter()
 
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun initAdapter() {
+        adapter = ListScreenAdapter()
+        listScreenBinding.recyclerView.adapter = adapter
+    }
+
+    private fun initViewModel() {
+
+        viewModel = ViewModelProvider(this).get(ListScreenViewModel::class.java)
+
+        checkNetworkStatus()
+
+        viewModel.getLiveData()
+            .observe(viewLifecycleOwner, Observer<ListItemModel> { listItemModel ->
+                if (listItemModel != null) {
+                    adapter.setUpdatedData(listItemModel.items)
+                } else {
+                    Toast.makeText(activity, "Error getting data", Toast.LENGTH_SHORT).show()
+                }
+            })
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun checkNetworkStatus() {
+
+        val conMgr =
+            activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as (ConnectivityManager)
+        val activeNetwork = conMgr.activeNetworkInfo;
+        if (activeNetwork != null && activeNetwork.isConnected) {
+            // notify user you are online
+            Toast.makeText(activity, "Network State: Online", Toast.LENGTH_SHORT).show()
+            makeAPICall()
+        } else {
+            // notify user you are not online
+            Toast.makeText(activity, "Network State: Offline", Toast.LENGTH_LONG).show()
+        }
+
+        networkChangeListener()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun networkChangeListener() {
+        val connectivityManager =
+            activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        connectivityManager.let {
+            it.registerDefaultNetworkCallback(object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    //take action when network connection is gained
+                    Toast.makeText(context, "Back Online", Toast.LENGTH_SHORT).show()
+                    makeAPICall()
+                }
+
+                override fun onLost(network: Network) {
+                    //take action when network connection is lost
+                    Toast.makeText(context, "You are Offline", Toast.LENGTH_LONG).show()
+                }
+            })
+        }
+    }
+
+    private fun makeAPICall() {
+        val query = ""
+        viewModel.sendQuery(query)
     }
 
     companion object {
