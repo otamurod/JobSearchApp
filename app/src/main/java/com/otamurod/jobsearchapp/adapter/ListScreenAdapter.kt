3a32d57be528f588a073778ddf16b7f06d7c955e
otamurod <o.safarov@student.inha.uz>
2022-10-04 22:06:57 +0500

    feat: create list screen adapter

---
+++ app/src/main/java/com/otamurod/jobsearchapp/adapter/ListScreenAdapter.kt
@@ -0,0 +1,24 @@
+package com.otamurod.jobsearchapp.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListScreenAdapter():RecyclerView.Adapter<ListScreenAdapter.VH>() {
+
+    inner class VH(itemView: View):RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
