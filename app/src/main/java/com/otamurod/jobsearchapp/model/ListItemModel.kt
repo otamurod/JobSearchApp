0f4e7506bc8c131c415ee21a8839633b922f8150
otamurod <o.safarov@student.inha.uz>
2022-10-04 23:31:08 +0500

    refactor: edit model class

--- app/src/main/java/com/otamurod/jobsearchapp/model/ListItemModel.kt
+++ app/src/main/java/com/otamurod/jobsearchapp/model/ListItemModel.kt
@@ -1,3 +1,4 @@
 package com.otamurod.jobsearchapp.model
 
-data class ListItemModel(val items: ArrayList<String>)
+data class ListItemModel(val locations: ArrayList<Location>)
+data class Location(val name: String)
