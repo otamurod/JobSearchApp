3000702848c4c8a7e271fdb67cfa3c949da831d3
otamurod <o.safarov@student.inha.uz>
2022-10-05 15:11:42 +0500

    refactor: clean up fragment

--- app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenFragment.kt
+++ app/src/main/java/com/otamurod/jobsearchapp/fragment/ListScreenFragment.kt
@@ -17,29 +17,7 @@ import com.otamurod.jobsearchapp.adapter.ListScreenAdapter
 import com.otamurod.jobsearchapp.databinding.FragmentListScreenBinding
 import com.otamurod.jobsearchapp.model.ListItemModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListScreenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListScreenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     //declare variables
     lateinit var listScreenBinding: FragmentListScreenBinding
     lateinit var viewModel: ListScreenViewModel
@@ -122,25 +100,5 @@ class ListScreenFragment : Fragment() {
     private fun makeAPICall() {
         val query = "page=1" //query to send to API
         viewModel.sendQuery(query) //send query
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListScreenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListScreenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
